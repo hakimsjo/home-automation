e6da1a0fc2d68d277a242eca09ab173486e55f04
Hakim Sjöström <hakimsjo@gmail.com>
2022-01-15 21:27:34 +0100

    Add files via upload

---
+++ Easee/GetChargerStatus.py
@@ -0,0 +1,36 @@
+import requests
+import clipboard
+import json
+import webbrowser
+import shortcuts
+
+url = "https://api.easee.cloud/api/accounts/login"
+
+payload = "{\"userName\":\"your@email.com\",\"password\":\"yourpassword\"}"
+headers = {
+	"Accept": "application/json",
+	"Content-Type": "application/*+json"
+}
+
+response = requests.request("POST", url, data=payload, headers=headers)
+
+#print(response.text)
+
+
+url = "https://api.easee.cloud/api/chargers/EH315754/state"
+
+headers = {
+	"Accept": "application/json",
+	"Authorization": "Bearer yourcode"
+}
+
+response = requests.request("GET", url, headers=headers)
+
+#print(response.text)
+
+clipboard.set(response.text)
+
+webbrowser.open('shortcuts://callback?callback=x-success')
+
+quit()
+
---
+++ Tibber/getTibberCurrentPrice.py
@@ -0,0 +1,28 @@
+import requests
+import clipboard
+import json
+import webbrowser
+
+myToken = 'yourtoken'
+headers = {'Authorization': "Bearer {}".format(myToken)}
+	
+# Prices now
+params = {"query": 
+  "{viewer {homes {currentSubscription{priceInfo{current{total energy tax startsAt}}}}}}"
+}
+
+r = requests.get('https://api.tibber.com/v1-beta/gql', headers=headers, params=params)
+
+j = r.text.replace('{"data":{"viewer":{"homes":[{"currentSubscription":{"priceInfo":{"current":{', ' ')
+
+j = j.replace('}}}}]}}}',' ')
+
+j = j.replace('"','')
+
+clipboard.set(j)
+
+webbrowser.open('shortcuts://callback?callback=x-success')
+
+quit()
+
+
