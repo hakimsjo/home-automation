df39f817251b1eea4eeb756bbfaec3ab683fae02
Hakim Sjöström <hakimsjo@gmail.com>
2022-01-17 07:56:30 +0100

    Add files via upload

--- Easee/GetChargerStatus.py
+++ Easee/GetChargerStatus.py
@@ -5,23 +5,27 @@ import webbrowser
 import shortcuts
 
 url = "https://api.easee.cloud/api/accounts/login"
-
+chargerID = "yourid"
 payload = "{\"userName\":\"your@email.com\",\"password\":\"yourpassword\"}"
+
 headers = {
 	"Accept": "application/json",
 	"Content-Type": "application/*+json"
 }
 
 response = requests.request("POST", url, data=payload, headers=headers)
-
 #print(response.text)
 
+d = json.loads(response.text)
+bearer = d.get("accessToken")
+#print(bearer)
 
-url = "https://api.easee.cloud/api/chargers/yourchargerid/state"
+chargerID = "yourid"
+url = "https://api.easee.cloud/api/chargers/" + chargerID + "/state"
 
 headers = {
 	"Accept": "application/json",
-	"Authorization": "Bearer yourcode"
+	"Authorization": "Bearer " + bearer
 }
 
 response = requests.request("GET", url, headers=headers)
